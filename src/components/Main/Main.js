ad9f18371292a68549c253635620f761c2b87516
Leonid <dolgodvorov.l@yandex.ru>
2022-10-13 20:10:54 +0500

    Созданы файлы основных компонентов, прописаны роуты

---
+++ src/components/Main/Main.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import './Main.css';
+import Header from "../Header/Header";
+import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
+import Footer from "../Footer/Footer";
+
+
+const Main = () => {
+  return (
+    <>
+      <div className="main">
+        <Header />      
+        <Promo />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Main;
